c96fd838c007f2dde33e3c469c207f012e70762d
OctavianoSoto <osoto.iv@gmail.com>
2022-08-24 08:14:00 -0500

    exercise complete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("Let's get this party started!");
 const rating = 'g'
 const api_key = 'gE3AZVm44jopff9IQ11JKrzkViOwYsZq'
-const resultsDiv = document.querySelector('#results');
+const resultsDiv = $('#results');
 
 $('form').on('submit', getGIF);
 $('#reset').on('click', resetGIF);
@@ -11,26 +11,26 @@ function getOffset() { return Math.floor(Math.random() * 51); }
 async function getGIF(e) {
     e.preventDefault();
     try {
+        //q is the val from the input. if input is empty...do nothing
         const q = $('#searchTerm').val();
         if (!q) { return }
         const offset = getOffset();
         const res = await axios.get('http://api.giphy.com/v1/gifs/search', { params: { api_key, q, offset, rating } });
-        const imgURL = res.data.data[0].images.fixed_height.url;
-        postImg(imgURL);
+        const { url } = res.data.data[0].images.fixed_height;
+        postGIF(url);
     }
     catch (e) {
         console.dir(e.message);
     }
 }
-function postImg(url) {
+function postGIF(url) {
+    //mixing normal DOM element with jQuery
+    //
     const image = document.createElement('img');
     image.src = url;
     resultsDiv.append(image);
 }
 
 function resetGIF() {
-    const children = resultsDiv.children;
-    for (let img of children) {
-        img.remove();
-    }
+    resultsDiv.empty();
 }
